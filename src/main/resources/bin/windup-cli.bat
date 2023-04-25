a113050ba681d64096bd690c553348449d9a8677
PhilipCattanach <pcattana@redhat.com>
2023-04-25 15:30:43 +0100

    WINDUPRULE-976 update version of Openrewrite used within CLI distribution to leverage new capabilities

--- src/main/resources/bin/windup-cli.bat
+++ src/main/resources/bin/windup-cli.bat
@@ -217,7 +217,7 @@ if %RUN_OPENREWRITE%==false goto runWINDUP_CLI
 
 :runOpenrewrite
 PUSHD "%OR_TRANSFORM_PATH%"
-mvn "org.openrewrite.maven:rewrite-maven-plugin:4.25.0:%OR_GOAL%"  %OR_CMD_LINE_ARGS%
+mvn "org.openrewrite.maven:rewrite-maven-plugin:4.44.0:%OR_GOAL%"  %OR_CMD_LINE_ARGS%
 POPD
 
 if ERRORLEVEL 1 goto error
